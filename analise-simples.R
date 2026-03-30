d82a9e6496f9c8158a9d239f1d99f14f6dcbd3ed
oceanocely <oliveiramarcely@id.uff.br>
2026-03-30 10:15:19 -0300

    verificando estrutura de dados e análise descritiva - vars. qualitativas

--- analise-simples.R
+++ analise-simples.R
@@ -1,2 +1,15 @@
 library(readr)
 dados = read.csv("dados - respostas formulário.csv", header = T, sep = ";")
+# verificando a estrutura dos dados
+attach(dados)
+str(dados)
+
+# análises descritivas - variáveis qualitativas
+prop.table(table(ingresso))
+prop.table(table(sexo))
+prop.table(table(grupo_sanguineo))
+prop.table(table(ingere_pao))
+prop.table(table(ingere_massa))
+prop.table(table(ingere_refri))
+prop.table(table(ingere_alcool))
+prop.table(table(fruta_gosta))
