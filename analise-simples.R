484809406d458bea4a7a358ad29710631eef470f
oceanocely <oliveiramarcely@id.uff.br>
2026-03-30 10:45:47 -0300

    Adicionando análise sobre frutas

--- analise-simples.R
+++ analise-simples.R
@@ -1,10 +1,11 @@
-library(readr)
+# FAZENDO A LEITURA DOS DADOS
 dados = read.csv("dados - respostas formulário.csv", header = T, sep = ";")
-# verificando a estrutura dos dados
+
+# VERIFICANDO A ESTRUTURA DOS DADOS
 attach(dados)
 str(dados)
 
-# análises descritivas - variáveis qualitativas
+# ANÁLISES DESCRITIVAS - VARIÁVEIS QUALITATIVAS
 prop.table(table(ingresso))
 prop.table(table(sexo))
 prop.table(table(grupo_sanguineo))
@@ -12,7 +13,27 @@ prop.table(table(ingere_pao))
 prop.table(table(ingere_massa))
 prop.table(table(ingere_refri))
 prop.table(table(ingere_alcool))
+
 prop.table(table(fruta_gosta))
+abacaxi = grepl("Abacaxi", fruta_gosta)
+prop.table(table(abacaxi))
+melancia = grepl("Melancia", fruta_gosta)
+prop.table(table(melancia))
+banana = grepl("Banana", fruta_gosta)
+prop.table(table(banana))
+pera = grepl("Pera", fruta_gosta)
+prop.table(table(pera))
+manga = grepl("Manga", fruta_gosta)
+prop.table(table(manga))
+maça = grepl("Maçã", fruta_gosta)
+prop.table(table(maça))
+kiwi = grepl("Kiwi", fruta_gosta)
+prop.table(table(kiwi))
+nenhuma = grepl("Nenhuma das frutas mencionadas", fruta_gosta)
+prop.table(table(nenhuma))
+
+unlist(strsplit(fruta_gosta, ", ")) #teste
+)
 
 # ANÁLISES DESCRITIVAS - VARIÁVEIS QUANTITATIVAS
 summary(idade)
@@ -20,3 +41,4 @@ summary(peso)
 sd(peso)
 summary(n_refeicoes_dia)
 sd(n_refeicoes_dia)
+
